4f0f317bb6238ca4c1661046b6642e21ad697cec
myl7 <myl@myl.moe>
2023-07-15 12:43:26 +0800

    Fix stack overflow for bench dcf_large_lambda

--- benches/dcf_large_lambda.rs
+++ benches/dcf_large_lambda.rs
@@ -5,27 +5,27 @@ use dcf::prg::Aes256HirosePrg;
 use dcf::{BoundState, CmpFn, Dcf, DcfImpl};
 
 pub fn bench(c: &mut Criterion) {
-    let mut keys = [[0; 32]; 2048];
+    let mut keys = vec![[0; 32]; 2048];
     keys.iter_mut().for_each(|key| thread_rng().fill_bytes(key));
     let prg = Aes256HirosePrg::<16384, 2048>::new(std::array::from_fn(|i| &keys[i]));
     let dcf = DcfImpl::<16, 16384, _>::new(prg);
-    let mut s0s = [[0; 16384]; 2];
+    let mut s0s = vec![[0; 16384]; 2];
     thread_rng().fill_bytes(&mut s0s[0]);
     thread_rng().fill_bytes(&mut s0s[1]);
-    let mut f = CmpFn {
+    let mut f = Box::new(CmpFn {
         alpha: thread_rng().gen(),
         beta: [0; 16384],
-    };
+    });
     thread_rng().fill_bytes(&mut f.beta);
     let k = dcf.gen(&f, [&s0s[0], &s0s[1]], BoundState::LtBeta);
     const N: usize = 10_000;
-    let xs: [[u8; 16]; N] = std::array::from_fn(|_| thread_rng().gen());
+    let xs: Vec<[u8; 16]> = (0..N).map(|_| thread_rng().gen()).collect();
 
     c.bench_function("xs_10k_lambda_16384", |b| {
         b.iter(|| {
             let prg = Aes256HirosePrg::<16384, 2048>::new(std::array::from_fn(|i| &keys[i]));
             let dcf = DcfImpl::<16, 16384, _>::new(prg);
-            let mut ys = [[0; 16384]; N];
+            let mut ys = vec![[0; 16384]; N];
             dcf.eval(
                 false,
                 &k,
