f724c8e16db9ad5342fca7fa4f170702a8fdb791
myl7 <myl@myl.moe>
2023-10-01 15:52:24 +0800

    Add DPF impl

---
+++ dpf-fss/src/lib.rs
@@ -0,0 +1,288 @@
+// Copyright (C) myl7
+// SPDX-License-Identifier: Apache-2.0
+
+//! See [`Dpf`]
+
+extern crate group_math as group;
+
+#[cfg(feature = "prg")]
+pub mod prg;
+
+use bitvec::prelude::*;
+use dcf::{CmpFn, Cw, Prg, Share};
+use group::byte::utils::{xor, xor_inplace};
+use group::Group;
+#[cfg(feature = "multithread")]
+use rayon::prelude::*;
+
+/// API of Distributed point function.
+///
+/// See [`CmpFn`] for `N` and `LAMBDA`.
+pub trait Dpf<const N: usize, const LAMBDA: usize, G>
+where
+    G: Group<LAMBDA>,
+{
+    /// `s0s` is `$s^{(0)}_0$` and `$s^{(0)}_1$` which should be randomly sampled
+    fn gen(&self, f: &CmpFn<N, LAMBDA, G>, s0s: [&[u8; LAMBDA]; 2]) -> Share<LAMBDA, G>;
+
+    /// `b` is the party. `false` is 0 and `true` is 1.
+    fn eval(&self, b: bool, k: &Share<LAMBDA, G>, xs: &[&[u8; N]], ys: &mut [&mut G]);
+}
+
+/// Point function.
+///
+/// - `N` is the **byte** size of the domain.
+/// - `LAMBDA` here is used as the **byte** size of the range, unlike the one in the paper.
+// pub struct CmpFn<const N: usize, const LAMBDA: usize, G>
+// where
+//     G: Group<LAMBDA>,
+// {
+//     /// `$\alpha$`
+//     pub alpha: [u8; N],
+//     /// `$\beta$`
+//     pub beta: G,
+// }
+
+/// Pseudorandom generator used in the algorithm.
+///
+/// `$\{0, 1\}^{\lambda} \rightarrow \{0, 1\}^{2(2\lambda + 1)}$`.
+// #[cfg(feature = "multithread")]
+// pub trait Prg<const LAMBDA: usize>: Sync {
+//     fn gen(&self, seed: &[u8; LAMBDA]) -> [([u8; LAMBDA], [u8; LAMBDA], bool); 2];
+// }
+// #[cfg(not(feature = "multithread"))]
+// pub trait Prg<const LAMBDA: usize> {
+//     fn gen(&self, seed: &[u8; LAMBDA]) -> [([u8; LAMBDA], [u8; LAMBDA], bool); 2];
+// }
+
+/// Implementation of [`Dpf`].
+///
+/// `$\alpha$` itself is not included, which means `$f(\alpha)$ = 0`.
+pub struct DpfImpl<const N: usize, const LAMBDA: usize, PrgT>
+where
+    PrgT: Prg<LAMBDA>,
+{
+    prg: PrgT,
+}
+
+impl<const N: usize, const LAMBDA: usize, PrgT> DpfImpl<N, LAMBDA, PrgT>
+where
+    PrgT: Prg<LAMBDA>,
+{
+    pub fn new(prg: PrgT) -> Self {
+        Self { prg }
+    }
+}
+
+const IDX_L: usize = 0;
+const IDX_R: usize = 1;
+
+impl<const N: usize, const LAMBDA: usize, PrgT, G> Dpf<N, LAMBDA, G> for DpfImpl<N, LAMBDA, PrgT>
+where
+    PrgT: Prg<LAMBDA>,
+    G: Group<LAMBDA>,
+{
+    fn gen(&self, f: &CmpFn<N, LAMBDA, G>, s0s: [&[u8; LAMBDA]; 2]) -> Share<LAMBDA, G> {
+        // The bit size of `$\alpha$`
+        let n = 8 * N;
+        // let mut v_alpha = G::zero();
+        let mut ss = Vec::<[[u8; LAMBDA]; 2]>::with_capacity(n + 1);
+        // Set `$s^{(1)}_0$` and `$s^{(1)}_1$`
+        ss.push([s0s[0].to_owned(), s0s[1].to_owned()]);
+        let mut ts = Vec::<[bool; 2]>::with_capacity(n + 1);
+        // Set `$t^{(0)}_0$` and `$t^{(0)}_1$`
+        ts.push([false, true]);
+        let mut cws = Vec::<Cw<LAMBDA, G>>::with_capacity(n);
+        for i in 1..n + 1 {
+            let [(s0l, _v0l, t0l), (s0r, _v0r, t0r)] = self.prg.gen(&ss[i - 1][0]);
+            let [(s1l, _v1l, t1l), (s1r, _v1r, t1r)] = self.prg.gen(&ss[i - 1][1]);
+            // MSB is required since we index from high to low in arrays
+            let alpha_i = f.alpha.view_bits::<Msb0>()[i - 1];
+            let (keep, lose) = if alpha_i {
+                (IDX_R, IDX_L)
+            } else {
+                (IDX_L, IDX_R)
+            };
+            let s_cw = xor(&[[&s0l, &s0r][lose], [&s1l, &s1r][lose]]);
+            let tl_cw = t0l ^ t1l ^ alpha_i ^ true;
+            let tr_cw = t0r ^ t1r ^ alpha_i;
+            let cw = Cw {
+                s: s_cw,
+                v: G::zero(),
+                tl: tl_cw,
+                tr: tr_cw,
+            };
+            cws.push(cw);
+            ss.push([
+                xor(&[
+                    [&s0l, &s0r][keep],
+                    if ts[i - 1][0] { &s_cw } else { &[0; LAMBDA] },
+                ]),
+                xor(&[
+                    [&s1l, &s1r][keep],
+                    if ts[i - 1][1] { &s_cw } else { &[0; LAMBDA] },
+                ]),
+            ]);
+            ts.push([
+                [t0l, t0r][keep] ^ (ts[i - 1][0] & [tl_cw, tr_cw][keep]),
+                [t1l, t1r][keep] ^ (ts[i - 1][1] & [tl_cw, tr_cw][keep]),
+            ]);
+        }
+        assert_eq!((ss.len(), ts.len(), cws.len()), (n + 1, n + 1, n));
+        let cw_np1 = (f.beta.clone()
+            + G::clone_convert(&ss[n][0]).add_inverse()
+            + G::clone_convert(&ss[n][1]))
+        .add_inverse_if(ts[n][1]);
+        Share {
+            s0s: vec![s0s[0].to_owned(), s0s[1].to_owned()],
+            cws,
+            cw_np1,
+        }
+    }
+
+    fn eval(&self, b: bool, k: &Share<LAMBDA, G>, xs: &[&[u8; N]], ys: &mut [&mut G]) {
+        let n = k.cws.len();
+        assert_eq!(n, N * 8);
+        let f = |x: &[u8; N], v: &mut G| {
+            let mut ss = Vec::<[u8; LAMBDA]>::with_capacity(n + 1);
+            ss.push(k.s0s[0].to_owned());
+            let mut ts = Vec::<bool>::with_capacity(n + 1);
+            ts.push(b);
+            for i in 1..n + 1 {
+                let cw = &k.cws[i - 1];
+                let [(mut sl, _vl_hat, mut tl), (mut sr, _vr_hat, mut tr)] =
+                    self.prg.gen(&ss[i - 1]);
+                xor_inplace(&mut sl, &[if ts[i - 1] { &cw.s } else { &[0; LAMBDA] }]);
+                xor_inplace(&mut sr, &[if ts[i - 1] { &cw.s } else { &[0; LAMBDA] }]);
+                tl ^= ts[i - 1] & cw.tl;
+                tr ^= ts[i - 1] & cw.tr;
+                if x.view_bits::<Msb0>()[i - 1] {
+                    ss.push(sr);
+                    ts.push(tr);
+                } else {
+                    ss.push(sl);
+                    ts.push(tl);
+                }
+            }
+            assert_eq!((ss.len(), ts.len()), (n + 1, n + 1));
+            *v = (G::convert(ss[n]) + if ts[n] { k.cw_np1.clone() } else { G::zero() })
+                .add_inverse_if(b);
+        };
+        #[cfg(feature = "multithread")]
+        {
+            xs.par_iter()
+                .zip(ys.par_iter_mut())
+                .for_each(|(x, y)| f(x, y));
+        }
+        #[cfg(not(feature = "multithread"))]
+        {
+            xs.iter().zip(ys.iter_mut()).for_each(|(x, y)| f(x, y));
+        }
+    }
+}
+
+/// `Cw`. Correclation word.
+// #[derive(Clone)]
+// pub struct Cw<const LAMBDA: usize, G>
+// where
+//     G: Group<LAMBDA>,
+// {
+//     pub s: [u8; LAMBDA],
+//     pub v: G,
+//     pub tl: bool,
+//     pub tr: bool,
+// }
+
+/// `k`.
+///
+/// `cws` and `cw_np1` is shared by the 2 parties.
+/// Only `s0s[0]` is different.
+// #[derive(Clone)]
+// pub struct Share<const LAMBDA: usize, G>
+// where
+//     G: Group<LAMBDA>,
+// {
+//     /// For the output of `gen`, its length is 2.
+//     /// For the input of `eval`, the first one is used.
+//     pub s0s: Vec<[u8; LAMBDA]>,
+//     /// The length of `cws` must be `n = 8 * N`
+//     pub cws: Vec<Cw<LAMBDA, G>>,
+//     /// `$CW^{(n + 1)}$`
+//     pub cw_np1: G,
+// }
+
+#[cfg(all(test, feature = "prg"))]
+mod tests {
+    use super::*;
+
+    use group::byte::ByteGroup;
+    use rand::{thread_rng, Rng};
+
+    use crate::prg::Aes256HirosePrg;
+
+    const KEYS: [&[u8; 32]; 2] = [
+        b"j9\x1b_\xb3X\xf33\xacW\x15\x1b\x0812K\xb3I\xb9\x90r\x1cN\xb5\xee9W\xd3\xbb@\xc6d",
+        b"\x9b\x15\xc8\x0f\xb7\xbc!q\x9e\x89\xb8\xf7\x0e\xa0S\x9dN\xfa\x0c;\x16\xe4\x98\x82b\xfcdy\xb5\x8c{\xc2",
+    ];
+    const ALPHAS: &[&[u8; 16]] = &[
+        b"K\xa9W\xf5\xdd\x05\xe9\xfc?\x04\xf6\xfbUo\xa8C",
+        b"\xc2GK\xda\xc6\xbb\x99\x98Fq\"f\xb7\x8csU",
+        b"\xc2GK\xda\xc6\xbb\x99\x98Fq\"f\xb7\x8csV",
+        b"\xc2GK\xda\xc6\xbb\x99\x98Fq\"f\xb7\x8csW",
+        b"\xef\x96\x97\xd7\x8f\x8a\xa4AP\n\xb35\xb5k\xff\x97",
+    ];
+    const BETA: &[u8; 16] = b"\x03\x11\x97\x12C\x8a\xe9#\x81\xa8\xde\xa8\x8f \xc0\xbb";
+
+    #[test]
+    fn test_dpf_gen_then_eval_ok() {
+        let prg = Aes256HirosePrg::new(KEYS);
+        let dpf = DpfImpl::<16, 16, _>::new(prg);
+        let s0s: [[u8; 16]; 2] = thread_rng().gen();
+        let f = CmpFn {
+            alpha: ALPHAS[2].to_owned(),
+            beta: ByteGroup::clone_convert(BETA),
+        };
+        let k = dpf.gen(&f, [&s0s[0], &s0s[1]]);
+        let mut k0 = k.clone();
+        k0.s0s = vec![k0.s0s[0]];
+        let mut k1 = k.clone();
+        k1.s0s = vec![k1.s0s[1]];
+        let mut ys0 = vec![ByteGroup::zero(); ALPHAS.len()];
+        let mut ys1 = vec![ByteGroup::zero(); ALPHAS.len()];
+        dpf.eval(false, &k0, ALPHAS, &mut ys0.iter_mut().collect::<Vec<_>>());
+        dpf.eval(true, &k1, ALPHAS, &mut ys1.iter_mut().collect::<Vec<_>>());
+        ys0.iter_mut()
+            .zip(ys1.iter())
+            .for_each(|(y0, y1)| *y0 += y1.clone());
+        ys1 = vec![
+            ByteGroup::zero(),
+            ByteGroup::zero(),
+            ByteGroup::clone_convert(BETA),
+            ByteGroup::zero(),
+            ByteGroup::zero(),
+        ];
+        assert_eq!(ys0, ys1);
+    }
+
+    #[test]
+    fn test_dpf_gen_then_eval_not_zeros() {
+        let prg = Aes256HirosePrg::new(KEYS);
+        let dpf = DpfImpl::<16, 16, _>::new(prg);
+        let s0s: [[u8; 16]; 2] = thread_rng().gen();
+        let f = CmpFn {
+            alpha: ALPHAS[2].to_owned(),
+            beta: ByteGroup::clone_convert(BETA),
+        };
+        let k = dpf.gen(&f, [&s0s[0], &s0s[1]]);
+        let mut k0 = k.clone();
+        k0.s0s = vec![k0.s0s[0]];
+        let mut k1 = k.clone();
+        k1.s0s = vec![k1.s0s[1]];
+        let mut ys0 = vec![ByteGroup::zero(); ALPHAS.len()];
+        let mut ys1 = vec![ByteGroup::zero(); ALPHAS.len()];
+        dpf.eval(false, &k0, ALPHAS, &mut ys0.iter_mut().collect::<Vec<_>>());
+        dpf.eval(true, &k1, ALPHAS, &mut ys1.iter_mut().collect::<Vec<_>>());
+        assert_ne!(ys0[2], ByteGroup::zero());
+        assert_ne!(ys1[2], ByteGroup::zero());
+    }
+}
---
+++ dpf-fss/src/prg.rs
@@ -0,0 +1,91 @@
+// Copyright (C) myl7
+// SPDX-License-Identifier: Apache-2.0
+
+//! [`crate::Prg`] implementations
+
+use aes::cipher::generic_array::GenericArray;
+use aes::cipher::{BlockEncrypt, KeyInit};
+use aes::Aes256;
+use bitvec::prelude::*;
+use group::byte::utils::{xor, xor_inplace};
+
+use crate::Prg;
+
+/// Hirose double-block-length one-way compression function with AES256 and precreated keys
+/// as an implementation of [`Prg`].
+///
+/// To avoid `#![feature(generic_const_exprs)]`, it is **your responsibility**
+/// to ensure `LAMBDA % 16 = 0` and `N = LAMBDA / 16`.
+///
+/// It actually works for LAMBDA * 4 - 1 bits other than LAMBDA bytes.
+/// The last bit of the output `[u8; LAMBDA]` is always set to 0.
+/// The second (`v`) outputs are always `[0; LAMBDA]`.
+pub struct Aes256HirosePrg<const LAMBDA: usize, const N: usize> {
+    ciphers: [Aes256; N],
+}
+
+impl<const LAMBDA: usize, const N: usize> Aes256HirosePrg<LAMBDA, N> {
+    pub fn new(keys: [&[u8; 32]; N]) -> Self {
+        let ciphers = std::array::from_fn(|i| {
+            let key_block = GenericArray::from_slice(keys[i]);
+            Aes256::new(key_block)
+        });
+        Self { ciphers }
+    }
+
+    /// Get the arbitrary non-zero constant c
+    fn c() -> [u8; LAMBDA] {
+        std::array::from_fn(|_| 0xff)
+    }
+}
+
+impl<const LAMBDA: usize, const N: usize> Prg<LAMBDA> for Aes256HirosePrg<LAMBDA, N> {
+    fn gen(&self, seed: &[u8; LAMBDA]) -> [([u8; LAMBDA], [u8; LAMBDA], bool); 2] {
+        // `$p(G_{i - 1})$`
+        let seed_p = xor(&[seed, &Self::c()]);
+        let mut result_buf0 = [[0; LAMBDA]; 2];
+        let mut out_blocks = [GenericArray::default(); 2];
+        (0..LAMBDA / 16).for_each(|j| {
+            let in_block0 = GenericArray::from_slice(&seed[j * 16..(j + 1) * 16]);
+            let in_block1 = GenericArray::from_slice(&seed_p[j * 16..(j + 1) * 16]);
+            self.ciphers[j]
+                .encrypt_blocks_b2b(&[*in_block0, *in_block1], &mut out_blocks)
+                .unwrap();
+            result_buf0[0][j * 16..(j + 1) * 16].copy_from_slice(out_blocks[0].as_ref());
+            result_buf0[1][j * 16..(j + 1) * 16].copy_from_slice(out_blocks[1].as_ref());
+        });
+        xor_inplace(&mut result_buf0[0], &[seed]);
+        xor_inplace(&mut result_buf0[1], &[&seed_p]);
+        let bit0 = result_buf0[0].view_bits::<Lsb0>()[0];
+        let bit1 = result_buf0[1].view_bits::<Lsb0>()[0];
+        result_buf0
+            .iter_mut()
+            .for_each(|buf| buf[LAMBDA - 1].view_bits_mut::<Lsb0>().set(0, false));
+        [
+            (result_buf0[0], [0; LAMBDA], bit0),
+            (result_buf0[1], [0; LAMBDA], bit1),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const KEYS: [&[u8; 32]; 1] =
+        [b"j9\x1b_\xb3X\xf33\xacW\x15\x1b\x0812K\xb3I\xb9\x90r\x1cN\xb5\xee9W\xd3\xbb@\xc6d"];
+    const SEED: &[u8; 16] = b"*L\x8f%y\x12Z\x94*E\x8f$+NH\x19";
+
+    #[test]
+    fn test_prg_gen_not_zeros() {
+        let prg = Aes256HirosePrg::<16, 1>::new(KEYS);
+        let out = prg.gen(SEED);
+        (0..2).for_each(|i| {
+            assert_ne!(out[i].0, [0; 16]);
+            // Passed but no need
+            // assert_eq!(out[i].1, [0; 16]);
+            assert_ne!(xor(&[&out[i].0, SEED]), [0; 16]);
+            assert_ne!(xor(&[&out[i].1, SEED]), [0; 16]);
+        });
+    }
+}
