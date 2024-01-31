4907bbd05dddb7d48fb60ff8c7cdd7235a338bd4
myl7 <myl@myl.moe>
2024-01-31 14:02:21 +0800

    Add dcf/dpf full domain eval bench

--- benches/dcf_full_eval.rs
+++ benches/dcf_full_eval.rs
@@ -12,7 +12,7 @@ use fss_rs::group::Group;
 pub fn bench(c: &mut Criterion) {
     let keys: [[u8; 32]; 2] = thread_rng().gen();
     let prg = Aes256HirosePrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
-    let dcf = DcfImpl::<16, 16, _>::new(prg);
+    let dcf = DcfImpl::<3, 16, _>::new(prg);
     let s0s: [[u8; 16]; 2] = thread_rng().gen();
     let f = CmpFn {
         alpha: thread_rng().gen(),
@@ -20,14 +20,23 @@ pub fn bench(c: &mut Criterion) {
         bound: BoundState::LtBeta,
     };
     let k = dcf.gen(&f, [&s0s[0], &s0s[1]]);
-    const N: usize = 100_000;
-    let mut xs = [[0; 16]; N];
-    xs.iter_mut().for_each(|x| *x = thread_rng().gen());
+    const N: usize = 2usize.pow(24);
 
-    c.bench_function("dcf eval 100k xs", |b| {
+    c.bench_function("dcf full eval 2^24 xs", |b| {
         b.iter(|| {
             let prg = Aes256HirosePrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
-            let dcf = DcfImpl::<16, 16, _>::new(prg);
+            let dcf = DcfImpl::<3, 16, _>::new(prg);
+            let mut ys = vec![ByteGroup::zero(); N];
+            dcf.full_eval(false, &k, &mut ys.iter_mut().collect::<Vec<_>>());
+        })
+    });
+
+    let mut xs = vec![[0; 3]; N];
+    xs.iter_mut().for_each(|x| *x = thread_rng().gen());
+    c.bench_function("dcf full eval batch 2^24 xs", |b| {
+        b.iter(|| {
+            let prg = Aes256HirosePrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
+            let dcf = DcfImpl::<3, 16, _>::new(prg);
             let mut ys = vec![ByteGroup::zero(); N];
             dcf.eval(
                 false,
@@ -39,5 +48,9 @@ pub fn bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = bench
+}
 criterion_main!(benches);
--- benches/dpf_full_eval.rs
+++ benches/dpf_full_eval.rs
@@ -12,21 +12,30 @@ use fss_rs::group::Group;
 pub fn bench(c: &mut Criterion) {
     let keys: [[u8; 16]; 2] = thread_rng().gen();
     let prg = Aes128MatyasMeyerOseasPrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
-    let dpf = DpfImpl::<16, 16, _>::new(prg);
+    let dpf = DpfImpl::<3, 16, _>::new(prg);
     let s0s: [[u8; 16]; 2] = thread_rng().gen();
     let f = PointFn {
         alpha: thread_rng().gen(),
         beta: ByteGroup(thread_rng().gen()),
     };
     let k = dpf.gen(&f, [&s0s[0], &s0s[1]]);
-    const N: usize = 100_000;
-    let mut xs = vec![[0; 16]; N];
-    xs.iter_mut().for_each(|x| *x = thread_rng().gen());
+    const N: usize = 2usize.pow(24);
 
-    c.bench_function("dpf eval 100k xs", |b| {
+    c.bench_function("dpf full eval 2^24 xs", |b| {
         b.iter(|| {
             let prg = Aes128MatyasMeyerOseasPrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
-            let dpf = DpfImpl::<16, 16, _>::new(prg);
+            let dpf = DpfImpl::<3, 16, _>::new(prg);
+            let mut ys = vec![ByteGroup::zero(); N];
+            dpf.full_eval(false, &k, &mut ys.iter_mut().collect::<Vec<_>>());
+        })
+    });
+
+    let mut xs = vec![[0; 3]; N];
+    xs.iter_mut().for_each(|x| *x = thread_rng().gen());
+    c.bench_function("dpf full eval batch 2^24 xs", |b| {
+        b.iter(|| {
+            let prg = Aes128MatyasMeyerOseasPrg::<16, 2>::new(std::array::from_fn(|i| &keys[i]));
+            let dpf = DpfImpl::<3, 16, _>::new(prg);
             let mut ys = vec![ByteGroup::zero(); N];
             dpf.eval(
                 false,
@@ -38,5 +47,9 @@ pub fn bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = bench
+}
 criterion_main!(benches);
