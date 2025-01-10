817d28b8b2ed6a433c1beec8148ec5d85e2621f4
myl7 <myl@myl.moe>
2025-01-10 19:06:26 +0800

    Add dpf api

--- include/dpf.h
+++ include/dpf.h
@@ -2,3 +2,24 @@
 // Copyright (C) 2025 Yulong Ming (myl7)
 
 #pragma once
+
+#include <stdint.h>
+#include <dpf_api.h>
+
+typedef struct {
+  uint8_t *bytes;
+  int bitlen;
+} Bits;
+
+typedef struct {
+  Bits alpha;
+  uint8_t *beta;
+} PointFunc;
+
+typedef struct {
+  uint8_t **cws;
+  uint8_t *cw_np1;
+} DpfKey;
+
+int dpf_gen(DpfKey k, const PointFunc pf, const uint8_t *s0s);
+int dpf_eval(uint8_t *y, uint8_t b, const DpfKey k, Bits x);
---
+++ include/dpf_api.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 Yulong Ming (myl7)
+
+#pragma once
+
+#include <stdint.h>
+
+#ifndef kLambda
+#define kLambda 16
+#endif
+
+int prg(uint8_t *out, const uint8_t *seed);
+
+void group_add(uint8_t *val, const uint8_t *rhs);
+void group_neg(uint8_t *val);
