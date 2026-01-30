8ecdebc69d6ba727bceac019b2bd6ffd52c54788
Yulong Ming <i@myl.moe>
2026-01-30 12:26:12 +0800

    Remove cw mac

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -17,15 +17,6 @@ target_compile_definitions(dcf_bytes_test PRIVATE -DkBlocks=4)
 target_link_libraries(dcf_bytes_test GTest::gtest_main dcf OpenSSL::Crypto)
 gtest_discover_tests(dcf_bytes_test)
 
-add_executable(
-  cw_mac_bytes_test src/cw_mac_bytes_test.cc
-  src/group/bytes.c
-  src/prg/aes128_mmo.c
-)
-target_compile_definitions(cw_mac_bytes_test PRIVATE -DkBlocks=4)
-target_link_libraries(cw_mac_bytes_test GTest::gtest_main cw_mac_bytes dpf dcf OpenSSL::Crypto)
-gtest_discover_tests(cw_mac_bytes_test)
-
 if(FSS_kLambda EQUAL 16)
   add_executable(
     dpf_u128_le_test src/dpf_test.cc
